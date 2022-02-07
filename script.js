701ce0c8956e7d3c6536020262f49d8b365f3b72
abhiyansh <abhiyansharya161@gmail.com>
2022-02-08 02:17:47 +0530

    add ui

--- script.js
+++ script.js
@@ -50,4 +50,78 @@ function cliGame(){
     else console.log('game over, you lose');
 }
 
-cliGame();
+function playOneRound(guessWord){
+    if(roundIndex==NUM_ROUNDS) return;
+    
+    guessWord = guessWord.toLowerCase();
+    
+    if(!checkValidity(actualWord, guessWord)){
+        alert('invalid length!');
+        return;
+    }
+    
+    let actualWordSet = new Set(actualWord);
+    
+    for(let i = 0; i<actualWord.length; i++){
+        let currDiv = actualGrid[roundIndex][i];
+        currDiv.textContent = guessWord.charAt(i);
+        if(actualWord.charAt(i)==guessWord.charAt(i)) currDiv.classList.add('letter-correct');
+        else if(actualWordSet.has(guessWord.charAt(i)))currDiv.classList.add('letter-exists');
+        else currDiv.classList.add('letter-incorrect');
+    }
+    
+    roundIndex++;
+
+    if(actualWord==guessWord) document.querySelector('.fina-result').textContent=`Game over, you win. Refresh page to play again`;
+    else if(roundIndex==NUM_ROUNDS) document.querySelector('.final-result').textContent=`Game over, you lose. Actual Word: ${actualWord}. Refresh page to play again`;
+}
+
+function onSubmit(event){
+    let input = document.querySelector('#input-guess');
+    playOneRound(input.value);
+    input.value = '';
+}
+
+function createGridCell(cell_width, cell_height){
+    const grid_cell = document.createElement('div');
+    
+    grid_cell.classList.add('grid-cell');
+    
+    grid_cell.style.width = cell_width+'px';
+    grid_cell.style.height = cell_height+'px';
+
+    return grid_cell
+}
+
+function createGrid(grid_container, m, n){
+    const actualGrid = Array.from(Array(m), () => new Array(n));
+    const cell_height = grid_container.clientHeight/m;
+    const cell_width = grid_container.clientWidth/n;
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            let gridCell = createGridCell(cell_width, cell_height);
+            grid_container.appendChild(gridCell);
+            actualGrid[i][j] = gridCell;
+        }
+    }
+    return actualGrid;
+}
+
+let WORD_LENGTH=5;
+let actualWord = generateRandomWord(words, WORD_LENGTH);
+const NUM_ROUNDS = 6;
+let roundIndex = 0;
+
+let input = document.querySelector('#input-guess');
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("submit-button").click();
+  }
+});
+
+let submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', onSubmit);
+
+const grid_container = document.querySelector('.grid-container');
+const actualGrid = createGrid(grid_container, NUM_ROUNDS, WORD_LENGTH);
